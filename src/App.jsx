05a584856adfbb520ded4a93346754b023736ddf
machiloop <hamidusodiq2@gmail.com>
2025-05-31 16:33:55 +0100

    feat(pkg): set up routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Login from "./pages/login";
 import AdminLogin from "./pages/admin/adminLogin";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="w-screen h-screen">
-      {/* <Login /> */}
-      <AdminLogin />
+      <Routes>
+        <Route path="/" index element={<Login />} />
+        <Route path="/admin" element={<AdminLogin />} />
+      </Routes>
     </div>
   );
 }
